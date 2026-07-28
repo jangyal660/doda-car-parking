fe1bf2011d1c71e25daf96946025a207c2deac38
aon-sabri <aon.sabri@gmail.com>
2026-07-28 14:23:50 +0500

    fixed bugs

--- __settings__.js
+++ __settings__.js
@@ -2,13 +2,13 @@ window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
 window.SCENE_PATH = "2557749.json";
 window.CONTEXT_OPTIONS = {
-    'antialias': true,
+    'antialias': false,
     'alpha': false,
     'preserveDrawingBuffer': false,
     'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "default"
 };
-window.SCRIPTS = [ 299959606, 299955852, 299951831, 299967892, 299937364, 299997627, 300005229, 300006526, 300221453, 300234895 ];
+window.SCRIPTS = [ 299959606, 299955852, 299951831, 299967892, 299937364, 299997627, 300005229, 300006526, 300234895, 300221453 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,

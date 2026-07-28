91dcb84b75444218b027382e9c4ced9021c7fd1f
aon-sabri <aon.sabri@gmail.com>
2026-07-28 14:59:38 +0500

    fixed bugs

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2557749.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 299959606, 299955852, 299951831, 299967892, 299937364, 299997627, 300005229, 300006526, 300234895, 300221453 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/299936621/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/299936625/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/299936616/1/ammo.js', 'preload' : true},
+];
